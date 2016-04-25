9a7b45de2445a0da34047d56212e977aa545d849
冷波 <1766658304@qq.com>
2016-04-25 09:35:00 +0800

    try 1 try

---
+++ Standard/src/Test/Test.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		float height=1.75f;
+		float weight=73;
+		System.out.println("the standard weight is  "+(height-1)*0.9*100+"kg");
+		System.out.println("the standard bust is  "+(height*100*0.535)+"cm");
+		System.out.println("the standard waistline is  "+(height*100*0.365)+"cm");
+		System.out.println("the standard hipline is  "+(height*100*0.565)+"cm");
+	}
+
+}
